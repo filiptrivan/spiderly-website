56a69594e2b7b9149cb7f1c34db07f8908219e39
filiptrivan <120225885+filiptrivan@users.noreply.github.com>
2025-04-22 02:58:19 +0200

    Setting up the dark Primeng theme.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,29 +4,11 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import Aura from '@primeng/themes/aura';
 
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
-import { definePreset } from '@primeng/themes';
 import { MessageService, ConfirmationService } from 'primeng/api';
-
-const MyPreset = definePreset(Aura, {
-  semantic: {
-      primary: {
-          50: '{pink.100}',
-          100: '{pink.200}',
-          200: '{pink.300}',
-          300: '{pink.400}',
-          400: '{pink.500}',
-          500: '{pink.600}',
-          600: '{pink.700}',
-          700: '{pink.800}',
-          800: '{pink.900}',
-          900: '{pink.950}',
-          950: '{pink.950}'
-      },
-  }
-});
+import { DarkThemePreset } from '../assets/dark-theme';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -34,7 +16,10 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: MyPreset,
+        preset: DarkThemePreset,
+        options: {
+          darkModeSelector: '.dark'
+        }
       }
     }),
     provideRouter(routes), 
