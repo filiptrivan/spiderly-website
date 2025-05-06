700f31e7fde00519ce4d25ef62fec6928530b4ec
dhinakaranst <dinastdhinast@.com>
2025-05-06 13:59:59 +0530

    Add custom 404 Not Found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { InMemoryScrollingOptions, Routes } from '@angular/router';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { PlaygroundComponent } from './pages/playground/playground.component';
 import { FAQComponent } from './pages/faq/faq.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'playground', component: PlaygroundComponent },
   { path: 'faq', component: FAQComponent },
-  { path: '**', redirectTo: '' },
+  { path: '404', component: NotFoundComponent },
+  { path: '**', component: NotFoundComponent },
 ];
 
 export const scrollConfig: InMemoryScrollingOptions = {
