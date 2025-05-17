e816cf6784f05778c3641362b4cdb7c4c6ac7b15
Filip Trivan <filiptrivan5@gmail.com>
2025-05-17 12:01:50 +0200

    Updated docs/:slug on server to achieve prerendering.

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'docs/:slug',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      const slugs = ['getting-started', 'attributes'];
+      return slugs.map(slug => ({ slug }));
+    },
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
