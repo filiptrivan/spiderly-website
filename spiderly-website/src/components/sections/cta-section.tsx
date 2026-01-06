a65cd09fea8ce7d2cabd4bef6e8e1cb072467bfc
filiptrivan <120225885+filiptrivan@users.noreply.github.com>
2026-01-06 02:45:16 +0100

    impr: Deleted Lamp component

--- spiderly-website/src/components/sections/cta-section.tsx
+++ spiderly-website/src/components/sections/cta-section.tsx
@@ -1,25 +1,22 @@
 import { AnimationContainer, MaxWidthWrapper } from '@/components';
 import { Button } from '@/components/ui/button';
-import { LampContainer } from '@/components/ui/lamp';
 import { ArrowRightIcon } from 'lucide-react';
 
 export const CTASection = () => {
   return (
     <MaxWidthWrapper>
       <AnimationContainer delay={0.1}>
-        <LampContainer>
-          <div className="flex flex-col items-center justify-center relative w-full text-center">
-            <h2 className="bg-gradient-to-b from-neutral-200 to-neutral-400 py-4 bg-clip-text text-center text-4xl md:text-7xl !leading-[1.15] font-medium font-heading tracking-tight text-transparent">
-              Try Spiderly Now.
-            </h2>
-            <div className="mt-6">
-              <Button>
-                Get started
-                <ArrowRightIcon className="w-4 h-4 ml-2" />
-              </Button>
-            </div>
+        <div className="flex flex-col items-center justify-center relative w-full text-center">
+          <h2 className="bg-gradient-to-b from-neutral-200 to-neutral-400 py-4 bg-clip-text text-center text-4xl md:text-7xl !leading-[1.15] font-medium font-heading tracking-tight text-transparent">
+            Try Spiderly Now.
+          </h2>
+          <div className="mt-6">
+            <Button>
+              Get started
+              <ArrowRightIcon className="w-4 h-4 ml-2" />
+            </Button>
           </div>
-        </LampContainer>
+        </div>
       </AnimationContainer>
     </MaxWidthWrapper>
   );
