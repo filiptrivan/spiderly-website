aaaa2d49fc408d1b70e90220bfb1f52f97513e86
filiptrivan <120225885+filiptrivan@users.noreply.github.com>
2026-01-08 17:18:13 +0100

    impr: Deleted all unused dependencies

--- spiderly-website/src/components/providers/providers.tsx
+++ spiderly-website/src/components/providers/providers.tsx
@@ -1,16 +1,13 @@
 'use client';
 
 import React from 'react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 interface Props {
   children: React.ReactNode;
 }
 
 const Providers = ({ children }: Props) => {
-  const client = new QueryClient();
-
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+  return <>{children}</>;
 };
 
 export default Providers;
