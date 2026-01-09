9860d328f96775b26e47a6e7784fe8fed8fc77a5
filiptrivan <120225885+filiptrivan@users.noreply.github.com>
2026-01-09 16:41:31 +0100

    impr: docs

--- spiderly-website/.source/source.config.mjs
+++ spiderly-website/.source/source.config.mjs
@@ -1,18 +1,9 @@
 // source.config.ts
-import { defineDocs, defineConfig } from "fumadocs-mdx/config";
+import { defineConfig, defineDocs } from "fumadocs-mdx/config";
 var { docs, meta } = defineDocs({
   dir: "content/docs"
 });
-var source_config_default = defineConfig({
-  mdxOptions: {
-    rehypeCodeOptions: {
-      themes: {
-        light: "github-light",
-        dark: "github-dark"
-      }
-    }
-  }
-});
+var source_config_default = defineConfig();
 export {
   source_config_default as default,
   docs,
--- spiderly-website/source.config.ts
+++ spiderly-website/source.config.ts
@@ -1,16 +1,7 @@
-import { defineDocs, defineConfig } from 'fumadocs-mdx/config';
+import { defineConfig, defineDocs } from 'fumadocs-mdx/config';
 
 export const { docs, meta } = defineDocs({
   dir: 'content/docs',
 });
 
-export default defineConfig({
-  mdxOptions: {
-    rehypeCodeOptions: {
-      themes: {
-        light: 'github-light',
-        dark: 'github-dark',
-      },
-    },
-  },
-});
+export default defineConfig();
