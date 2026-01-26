3c247081cad435567626b840e9b8f892a698f170
filiptrivan <120225885+filiptrivan@users.noreply.github.com>
2026-01-26 19:02:03 +0100

    fix: docs search

--- .source/server.ts
+++ .source/server.ts
@@ -1,38 +1,24 @@
 // @ts-nocheck
+import { default as __fd_glob_11 } from "../content/docs/meta.json?collection=meta"
+import * as __fd_glob_10 from "../content/docs/validation.mdx?collection=docs"
+import * as __fd_glob_9 from "../content/docs/translation.mdx?collection=docs"
+import * as __fd_glob_8 from "../content/docs/set-up-google-authentication.mdx?collection=docs"
+import * as __fd_glob_7 from "../content/docs/set-up-emailing.mdx?collection=docs"
+import * as __fd_glob_6 from "../content/docs/index.mdx?collection=docs"
+import * as __fd_glob_5 from "../content/docs/getting-started.mdx?collection=docs"
+import * as __fd_glob_4 from "../content/docs/frontend-customization.mdx?collection=docs"
+import * as __fd_glob_3 from "../content/docs/backend-customization.mdx?collection=docs"
+import * as __fd_glob_2 from "../content/docs/authorization.mdx?collection=docs"
+import * as __fd_glob_1 from "../content/docs/attributes.mdx?collection=docs"
+import * as __fd_glob_0 from "../content/docs/add-new-entity.mdx?collection=docs"
 import { server } from 'fumadocs-mdx/runtime/server';
-import * as __fd_glob_0 from '../content/docs/add-new-entity.mdx?collection=docs';
-import * as __fd_glob_1 from '../content/docs/attributes.mdx?collection=docs';
-import * as __fd_glob_2 from '../content/docs/authorization.mdx?collection=docs';
-import * as __fd_glob_3 from '../content/docs/backend-customization.mdx?collection=docs';
-import * as __fd_glob_4 from '../content/docs/frontend-customization.mdx?collection=docs';
-import * as __fd_glob_5 from '../content/docs/getting-started.mdx?collection=docs';
-import * as __fd_glob_6 from '../content/docs/index.mdx?collection=docs';
-import { default as __fd_glob_11 } from '../content/docs/meta.json?collection=meta';
-import * as __fd_glob_7 from '../content/docs/set-up-emailing.mdx?collection=docs';
-import * as __fd_glob_8 from '../content/docs/set-up-google-authentication.mdx?collection=docs';
-import * as __fd_glob_9 from '../content/docs/translation.mdx?collection=docs';
-import * as __fd_glob_10 from '../content/docs/validation.mdx?collection=docs';
 import type * as Config from '../source.config';
 
-const create = server<
-  typeof Config,
-  import('fumadocs-mdx/runtime/types').InternalTypeConfig & {
-    DocData: {};
+const create = server<typeof Config, import("fumadocs-mdx/runtime/types").InternalTypeConfig & {
+  DocData: {
   }
->({ doc: { passthroughs: ['extractedReferences'] } });
+}>({"doc":{"passthroughs":["extractedReferences"]}});
 
-export const docs = await create.doc('docs', 'content/docs', {
-  'add-new-entity.mdx': __fd_glob_0,
-  'attributes.mdx': __fd_glob_1,
-  'authorization.mdx': __fd_glob_2,
-  'backend-customization.mdx': __fd_glob_3,
-  'frontend-customization.mdx': __fd_glob_4,
-  'getting-started.mdx': __fd_glob_5,
-  'index.mdx': __fd_glob_6,
-  'set-up-emailing.mdx': __fd_glob_7,
-  'set-up-google-authentication.mdx': __fd_glob_8,
-  'translation.mdx': __fd_glob_9,
-  'validation.mdx': __fd_glob_10,
-});
+export const docs = await create.doc("docs", "content/docs", {"add-new-entity.mdx": __fd_glob_0, "attributes.mdx": __fd_glob_1, "authorization.mdx": __fd_glob_2, "backend-customization.mdx": __fd_glob_3, "frontend-customization.mdx": __fd_glob_4, "getting-started.mdx": __fd_glob_5, "index.mdx": __fd_glob_6, "set-up-emailing.mdx": __fd_glob_7, "set-up-google-authentication.mdx": __fd_glob_8, "translation.mdx": __fd_glob_9, "validation.mdx": __fd_glob_10, });
 
-export const meta = await create.meta('meta', 'content/docs', { 'meta.json': __fd_glob_11 });
+export const meta = await create.meta("meta", "content/docs", {"meta.json": __fd_glob_11, });
